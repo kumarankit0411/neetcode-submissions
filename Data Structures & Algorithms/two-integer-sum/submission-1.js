f2602b37f8ab6e9698becea742de229454943f9a
Ankit Kr. Singh <ankitsingh095@outlook.com>
2026-07-10 11:46:03 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // nums.forEach((num1, i) => {
+        //     if(output.length) return;
+        //     nums.forEach((num2, j) => {
+        //         if(i!=j && num1+num2 == target) {
+        //             output.push(i);
+        //             output.push(j);
+        //             return;
+        //         }
+        //     })
+        // })
+        for(let i = 0; i < nums.length; i++) {
+            for(let j = i+1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) return [i, j]
+            }
+        }
+    }
+}
